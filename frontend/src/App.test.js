ecc2130a63d3d83b74a9eeb0670d5d73c1519849
dssudake <darshansudake555@gmail.com>
2020-12-08 09:45:59 +0530

    Jest and Enzyme config

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  test('Render gretting', () => {
+    render(<App />);
+    const headElement = screen.getByText(/Welcome to AudMIX!/i);
+    expect(headElement).toBeInTheDocument();
+  });
+
+  test('Render greeting enzyme', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').text()).toEqual('Welcome to AudMIX!');
+  });
 });
