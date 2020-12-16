adf4d781c48aac5886089536c1205abb46459647
dssudake <darshansudake555@gmail.com>
2020-12-16 17:28:54 +0530

    Updated Frontend Upload Page

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -4,37 +4,49 @@ import { Container, Row, ButtonGroup, Button } from 'react-bootstrap';
 import { BsUpload } from 'react-icons/bs';
 import { BiArrowBack } from 'react-icons/bi';
 import { CgBrowse } from 'react-icons/cg';
+import { AiOutlineClose } from 'react-icons/ai';
 import axios from 'axios';
+
 import logo from '../assets/img/logo.png';
 
 export default function Upload() {
   useEffect(() => {
     document.title = 'Upload | AudMIX';
   });
-  const [fileName, setfileName] = useState('No file selected');
-  const [File, setfile] = useState();
+
+  const [isFileSelected, setFileSelected] = useState(false);
+  const [File, setfile] = useState(null);
+
   function namer(name) {
     var len = name.length;
     var d = new Date();
     var ext = name.slice(0, len - 4) + '_' + d.getTime();
     return ext;
   }
+
+  function formatBytes(bytes, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  }
+
   function handleFileBrowse() {
     const fileSelector = document.createElement('input');
     fileSelector.setAttribute('type', 'file');
     fileSelector.setAttribute('accept', 'audio/*');
     fileSelector.click();
     fileSelector.addEventListener('change', function (event) {
-      document.getElementById('upload_button').disabled = false;
-      var File = event.target.files[0];
-      var filename = namer(File.name);
-      setfileName(filename);
-      setfile(File);
+      var file = event.target.files[0];
+      setfile(file);
+      setFileSelected(true);
     });
   }
-  const upload = () => {
-    console.log(File.name);
-    console.log(fileName);
+
+  const handelUpload = () => {
+    var fileName = namer(File.name);
     const uploadData = new FormData();
     uploadData.append('Name', fileName);
     uploadData.append('File', File, File.name);
@@ -44,18 +56,47 @@ export default function Upload() {
       .catch((error) => console.log(error));
     alert('File Uploaded');
   };
+
   return (
     <Container fluid="xl" style={{ paddingTop: '20vh' }}>
       <Row className="justify-content-center">
         <img src={logo} width="200" />
       </Row>
+
       <Row className="justify-content-center text-secondary my-5 h2">Upload Audio or Video</Row>
-      <Row className="justify-content-center text-primary mb-4 h5">{fileName}</Row>
+
+      <Row className="justify-content-center text-primary mb-4 h5">
+        <ButtonGroup>
+          <Button variant="outline-secondary" className="bg-dark text-secondary px-5">
+            {!isFileSelected ? (
+              <>Browse & Select File to Upload</>
+            ) : (
+              <>
+                {File.name}
+                <br />
+                {formatBytes(File.size)}
+              </>
+            )}
+          </Button>
+          {isFileSelected && (
+            <Button
+              variant="outline-secondary"
+              onClick={() => {
+                setfile(null);
+                setFileSelected(false);
+              }}
+            >
+              <AiOutlineClose />
+            </Button>
+          )}
+        </ButtonGroup>
+      </Row>
+
       <Row className="justify-content-center">
         <ButtonGroup horizontal="true">
           <Button
             size="lg"
-            id="browse_button"
+            disabled={isFileSelected}
             variant="outline-primary"
             onClick={() => handleFileBrowse()}
           >
@@ -63,17 +104,16 @@ export default function Upload() {
             Browse
           </Button>
           <Button
-            type={Button}
-            disabled={false}
             size="lg"
-            variant="outline-primary"
-            onClick={() => upload()}
-            id="upload_button"
+            disabled={!isFileSelected}
+            variant="outline-secondary"
+            onClick={() => handelUpload()}
           >
             <BsUpload /> <br /> Upload
           </Button>
         </ButtonGroup>
       </Row>
+
       <Row className="justify-content-center mt-5">
         <Link to="/" className="border-bottom border-secondary text-secondary">
           <BiArrowBack /> Back to Home
