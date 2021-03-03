91e31fd99c6382b0d85a6adab24f97cec416b7e7
dssudake <darshansudake555@gmail.com>
2021-03-03 15:17:44 +0530

    Redirect Page after upload with counter

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { Container, Row, ButtonGroup, Button } from 'react-bootstrap';
 import { BsUpload } from 'react-icons/bs';
 import { BiArrowBack } from 'react-icons/bi';
@@ -14,26 +14,36 @@ export default function Upload() {
     document.title = 'Upload | AudMIX';
   });
 
+  const history = useHistory();
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [counter, setCounter] = React.useState(0);
+  const [id, setId] = useState('');
+  useEffect(() => {
+    if (isUploaded) {
+      counter > 0 ? setTimeout(() => setCounter(counter - 1), 1000) : history.push('/edit/' + id);
+    }
+  }, [counter]);
+
   const [isFileSelected, setFileSelected] = useState(false);
   const [File, setfile] = useState(null);
 
-  function namer(name) {
+  const namer = (name) => {
     var len = name.length;
     var d = new Date();
     var ext = name.slice(0, len - 4) + '_' + d.getTime();
     return ext;
-  }
+  };
 
-  function formatBytes(bytes, decimals = 2) {
+  const formatBytes = (bytes, decimals = 2) => {
     if (bytes === 0) return '0 Bytes';
     const k = 1024;
     const dm = decimals < 0 ? 0 : decimals;
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-  }
+  };
 
-  function handleFileBrowse() {
+  const handleFileBrowse = () => {
     const fileSelector = document.createElement('input');
     fileSelector.setAttribute('type', 'file');
     fileSelector.setAttribute('accept', 'audio/*,video/*');
@@ -43,18 +53,23 @@ export default function Upload() {
       setfile(file);
       setFileSelected(true);
     });
-  }
+  };
 
   const handelUpload = () => {
     var fileName = namer(File.name);
     const uploadData = new FormData();
     uploadData.append('Name', fileName);
-    uploadData.append('File', File, File.name);
+    uploadData.append('File', File, fileName);
     api
       .post('upload/', uploadData, {})
-      .then((res) => console.log(res))
+      .then((res) => {
+        if (res.status === 201) {
+          setIsUploaded(true);
+          setCounter(6);
+          setId(res.data.id);
+        }
+      })
       .catch((error) => console.log(error));
-    alert('File Uploaded');
   };
 
   return (
@@ -64,61 +79,71 @@ export default function Upload() {
       </Row>
 
       <Row className="justify-content-center text-secondary my-5 h2">Upload Audio or Video</Row>
+      {isUploaded ? (
+        <Row className="justify-content-center text-center text-primary mb-4 h5">
+          File successfully uploaded.
+          <br />
+          <br />
+          Redirecting you in {counter === 0 ? '0' : counter} sec...
+        </Row>
+      ) : (
+        <>
+          <Row className="justify-content-center text-primary mb-4 h5">
+            <ButtonGroup>
+              <Button variant="outline-secondary" className="bg-dark text-secondary px-5">
+                {!isFileSelected ? (
+                  <>Browse & Select File to Upload</>
+                ) : (
+                  <>
+                    {File.name}
+                    <br />
+                    {formatBytes(File.size)}
+                  </>
+                )}
+              </Button>
+              {isFileSelected && (
+                <Button
+                  variant="outline-secondary"
+                  onClick={() => {
+                    setfile(null);
+                    setFileSelected(false);
+                  }}
+                >
+                  <AiOutlineClose />
+                </Button>
+              )}
+            </ButtonGroup>
+          </Row>
 
-      <Row className="justify-content-center text-primary mb-4 h5">
-        <ButtonGroup>
-          <Button variant="outline-secondary" className="bg-dark text-secondary px-5">
-            {!isFileSelected ? (
-              <>Browse & Select File to Upload</>
-            ) : (
-              <>
-                {File.name}
-                <br />
-                {formatBytes(File.size)}
-              </>
-            )}
-          </Button>
-          {isFileSelected && (
-            <Button
-              variant="outline-secondary"
-              onClick={() => {
-                setfile(null);
-                setFileSelected(false);
-              }}
-            >
-              <AiOutlineClose />
-            </Button>
-          )}
-        </ButtonGroup>
-      </Row>
+          <Row className="justify-content-center">
+            <ButtonGroup horizontal="true">
+              <Button
+                size="lg"
+                disabled={isFileSelected}
+                variant="outline-primary"
+                onClick={() => handleFileBrowse()}
+              >
+                <CgBrowse /> <br />
+                Browse
+              </Button>
+              <Button
+                size="lg"
+                disabled={!isFileSelected}
+                variant="outline-secondary"
+                onClick={() => handelUpload()}
+              >
+                <BsUpload /> <br /> Upload
+              </Button>
+            </ButtonGroup>
+          </Row>
 
-      <Row className="justify-content-center">
-        <ButtonGroup horizontal="true">
-          <Button
-            size="lg"
-            disabled={isFileSelected}
-            variant="outline-primary"
-            onClick={() => handleFileBrowse()}
-          >
-            <CgBrowse /> <br />
-            Browse
-          </Button>
-          <Button
-            size="lg"
-            disabled={!isFileSelected}
-            variant="outline-secondary"
-            onClick={() => handelUpload()}
-          >
-            <BsUpload /> <br /> Upload
-          </Button>
-        </ButtonGroup>
-      </Row>
-
-      <Row className="justify-content-center mt-5">
-        <Link to="/" className="border-bottom border-secondary text-secondary">
-          <BiArrowBack /> Back to Home
-        </Link>
-      </Row>
+          <Row className="justify-content-center mt-5">
+            <Link to="/" className="border-bottom border-secondary text-secondary">
+              <BiArrowBack /> Back to Home
+            </Link>
+          </Row>
+        </>
+      )}
     </Container>
   );
 }
