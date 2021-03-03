95c5378974b5aa6e8ca8fc9d91742f553b72401f
dssudake <darshansudake555@gmail.com>
2021-03-03 15:14:34 +0530

    API Modifications

--- backend/file_upload/models.py
+++ backend/file_upload/models.py
@@ -4,4 +4,5 @@ import uuid
 class upload(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     Name=models.CharField(max_length=200)
+    UploadedAt = models.DateTimeField(auto_now_add=True)
     File=models.FileField(upload_to='user_media/%Y/%m/%d')
